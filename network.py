dff73cb1d406c478ba5ab9f2acbc556e94a1c693
alban-pierre <alban.pierre@ens.fr>
2017-03-24 17:36:58 +0100

    Add all arguments of Network in the contructor

--- network.py
+++ network.py
@@ -3,22 +3,22 @@ import math
 
 class Network:
 
-	def __init__(self, N=100, initial_mean=1, initial_variance=1):
+	def __init__(self, N=100, theta=0.2, f=0.5, H0=0.2, H1=0.2, lmbda=0.9, gamma=0.5, initial_mean=1, initial_variance=1): # Not very beautiful ...
 		self.N = N
+		self.theta = theta
+		self.f = f
+		self.H0 = H0
+		self.H1 = H1
+		self.lmbda = lmbda
+		self.gamma = gamma
 		self.W = initial_variance*numpy.random.randn(self.N, self.N) + initial_mean
 		numpy.fill_diagonal(self.W, 0)
 		self.W = numpy.maximum(self.W, 0)
-		self.theta = 0.2
 		self.activation_func = lambda x: (x > self.theta) + 0 # Later we can generalise the activation function as an argument given by the user
 		self.s = (np.random.random(self.N) > 0.5) + 0 # Is there any casting method better than "+0" ?
-		self.f = 0.5
-		self.H0 = 0.2
-		self.H1 = 0.2
-		self.lmbda = 0.9
-		self.gamma = 0.5
 		self.vi = np.ones(self.N)*self.theta
 
-	def update_states(self, xi):
+	def update_states(self, xi=np.zeros(1)):
 		inhibition = self.H0 + self.H1*xi.sum()/(self.f*self.N*self.gamma*math.sqrt(self.N)) + self.lmbda*(self.s.sum() - self.f*self.N)
 		self.vi = np.dot(self.W, self.s) + xi - inhibition
 		self.s = self.activation_func(self.vi)
@@ -28,6 +28,5 @@ class Network:
 		theta1 = self.theta + (self.gamma + epsilon)*self.f*math.sqrt(self.N)
 		coeffs = - (self.vi < self.theta) + (self.vi > self.theta0) + (self.vi > self.theta) - (self.vi < self.theta1)
 		self.W = self.W + coeffs*learn_rate*self.s
-
 
 	
