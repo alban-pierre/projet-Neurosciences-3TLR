83ea91dea2c3a6839e218f22dbd93305feb9e148
alban-pierre <alban.pierre@ens.fr>
2017-03-24 18:51:38 +0100

    Add training and testing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+import random
 
 N = 100
 theta = 0.2
@@ -7,13 +8,49 @@ f = 0.5
 H0 = 0.2
 H1 = 0.2
 lmbda = 0.9
-gamma = 0.5
+gamma = 6
 
 nbr_patterns = 10
+learning_length = 100
+learn_rate = 0.1
+epsilon = 1.2
+
+execfile('network.py')
+
+hamming_distance = lambda x, y: float(abs(x-y).sum())/x.size
 
 NN = Network(N, theta, f, H0, H1, lmbda, gamma)
 
-patterns = (np.random.rand((nbr_patterns, N)) < f) + 0
+patterns = (np.random.rand(nbr_patterns, N) < f) + 0
 
 
+#for ip in range(patterns.shape[0]):
+for i in range(learning_length):
+	ip = random.randint(0, nbr_patterns-1)
+	NN.update_states(patterns[ip, :])
+	NN.update_weights(learn_rate, epsilon)
 
+
+test_length = 1
+b = 0.1
+err = 0
+for i in range(test_length):
+	ip = random.randint(0, nbr_patterns-1)
+	p = np.copy(patterns[ip, :])
+	r = range(N)
+	random.shuffle(r)
+	r = r[:int(round(nbr_patterns*N))]
+	p[r] = (np.random.rand(round(nbr_patterns*N)) < f) + 0
+
+	NN.s = p
+	for j in range(30):
+		NN.update_states()
+	
+	d = hamming_distance(patterns[ip, :], NN.s)
+	err = err + (d > 0.01)
+
+
+	
+err = err/test_length
+
+print "The percentage of recovery at size {} is {}%".format(b, 100-err*100)
--- network.py
+++ network.py
@@ -11,9 +11,9 @@ class Network:
 		self.H1 = H1
 		self.lmbda = lmbda
 		self.gamma = gamma
-		self.W = initial_variance*numpy.random.randn(self.N, self.N) + initial_mean
-		numpy.fill_diagonal(self.W, 0)
-		self.W = numpy.maximum(self.W, 0)
+		self.W = initial_variance*np.random.randn(self.N, self.N) + initial_mean
+		np.fill_diagonal(self.W, 0)
+		self.W = np.maximum(self.W, 0)
 		self.activation_func = lambda x: (x > self.theta) + 0 # Later we can generalise the activation function as an argument given by the user
 		self.s = (np.random.random(self.N) > 0.5) + 0 # Is there any casting method better than "+0" ?
 		self.vi = np.ones(self.N)*self.theta
@@ -26,7 +26,6 @@ class Network:
 	def update_weights(self, learn_rate, epsilon):
 		theta0 = self.theta - (self.gamma + epsilon)*self.f*math.sqrt(self.N)
 		theta1 = self.theta + (self.gamma + epsilon)*self.f*math.sqrt(self.N)
-		coeffs = - (self.vi < self.theta) + (self.vi > self.theta0) + (self.vi > self.theta) - (self.vi < self.theta1)
+		coeffs = - (self.vi < self.theta) + (self.vi > theta0) + (self.vi > self.theta) - (self.vi < theta1)
 		self.W = self.W + coeffs*learn_rate*self.s
 
-	
