6eeb45f4ced46d600e473098c7603393ffa75e9a
John Zarka <jzarka@MacBook-Pro-de-John-Zarka.local>
2017-03-30 02:04:14 +0200

    Some changes

--- main.py
+++ main.py
@@ -1,54 +1,51 @@
 import numpy as np
-import math
 import random
+import network
 
+#Define Neural Network parameters
 N = 1001
 theta = 350
 f = 0.5
-lmbda = 1.08
 gamma = 6
 
+#Define Patterns to be learnt
+learning_length=100
 nbr_patterns = 10
-learning_length = 100
+patterns = (np.random.rand(nbr_patterns, N) < f).astype(int)
+
+#Define learning parameters
 learn_rate = 0.01
-epsilon = 1.2
+eps = 1.2 #robustness
 
-execfile('network.py')
-
+#Hamming distance
 hamming_distance = lambda x, y: float(abs(x-y).sum())/x.size
 
-NN = Network(N, theta, f, lmbda, gamma)
+#Initiliaze Neural Network
+NN = network.Network(N, theta, f, gamma)
 
-patterns = (np.random.rand(nbr_patterns, N) < f) + 0
-
-
-#for ip in range(patterns.shape[0]):
-for i in range(learning_length):
-	ip = random.randint(0, nbr_patterns-1)
-	NN.update_states(patterns[ip, :])
-	NN.update_weights(learn_rate, epsilon)
+#Learn patterns
+for i_pattern in range(patterns.shape[0]):
+	NN.Three_TLR(patterns[i_pattern], learn_rate, eps, nb_iter=learning_length)
 
 
+#Test storage capacity as a function of basin size
 test_length = 100
-b = 0.1
-err = 0
-for i in range(test_length):
-	ip = random.randint(0, nbr_patterns-1)
-	p = np.copy(patterns[ip, :])
-	r = range(N)
-	random.shuffle(r)
-	r = r[:int(round(N*b))]
-	p[r] = (np.random.rand(round(N*b)) < f) + 0
-
-	NN.s = p
-	for j in range(30):
-		NN.update_states()
-	
-	d = hamming_distance(patterns[ip, :], NN.s)
-	err = err + (d > 0.01)
-
-
-	
-err = err/test_length
+b = 0.1 #basin size
+successful_storage=np.zeros(patterns.shape[0]) #successful storage at basin size for the various patterns
+successful_storage_rate=0.9
+for i_pattern in range(patterns.shape[0]):
+    err=0
+    for i in range(test_length):
+        pat = np.copy(patterns[i_pattern])
+        random_part = range(N)
+        random.shuffle(random_part)
+        random_part = random_part[:int(round(N*b))]
+        pat[random_part] = (np.random.rand(int(round(N*b))) < f).astype(int)
+        NN.s = pat
+        NN.update_states(np.zeros(N), nb_iter=30)
+        d = hamming_distance(patterns[i_pattern], NN.s)
+        err = err + int((d > 0.01))
+    err = err/test_length
+    successful_storage[i_pattern]=int((1-err>successful_storage_rate))
 
 print "The percentage of recovery at size {} is {}%".format(b, 100-err*100)
--- network.py
+++ network.py
@@ -1,31 +1,47 @@
 import numpy as np
+from scipy.special import erfcinv
 import math
 
 class Network:
 
-	def __init__(self, N=100, theta=0.2, f=0.5, lmbda=0.9, gamma=0.5, initial_mean=1, initial_variance=1): # Not very beautiful ...
-		self.N = N
-		self.theta = float(theta)
-		self.f = float(f)
-		self.lmbda = float(lmbda)
-		self.gamma = float(gamma)
-		self.W = initial_variance*np.random.randn(self.N, self.N) + initial_mean
+    #Initialize network and define binary neural network attributes for the 3TLR 
+	def __init__(self, N=1001, theta=350, f=0.5, gamma=6, initial_mean=1, initial_variance=1): 
+		self.N = N #Number of neurons in the network (binary Mac Culloch Pitts model)
+		self.theta = float(theta) #Threshold in the step function to output post synaptic activity
+		self.f = float(f) #Sparsity of the network (i.e percentage of active neurons)
+		self.gamma = float(gamma) #Measures the strength of the external input to separate ON and OFF neurons in the first stage of the learning
+        #Weight matrix - initialized as Gaussian with 1 mean and 1 variance, 0 diagonal weights (no self-connection), then floored to zero (positive weights to respect Dale's principle)
+		self.W = initial_variance*np.random.randn(self.N, self.N) + initial_mean 
 		np.fill_diagonal(self.W, 0)
 		self.W = np.maximum(self.W, 0)
-		self.activation_func = lambda x: (x > self.theta) + 0 # Later we can generalise the activation function as an argument given by the user
-		self.s = (np.random.random(self.N) > 0.5) + 0 # Is there any casting method better than "+0" ?
-		self.vi = np.ones(self.N)*self.theta
+		self.activation_func = lambda x: (x > self.theta).astype(int) #Step activation function
+		self.s = (np.random.random(self.N) < self.f).astype(int) #State of the network - initialized as random {0,1} states with f sparseness
+		self.mask = np.ones(self.W.shape, dtype=bool) #mask to compute mean and standard deviation of the weight matrix (ignoring diagonal which are non random since set to 0)
+		np.fill_diagonal(self.mask, 0)
+		self.lmbda = self.W[self.mask].mean()
+		self.H0 = (self.N-1)*(self.f*self.W[self.mask].mean() - self.theta/(self.N-1)) + self.W[self.mask].std()*math.sqrt(2)*erfcinv(2*f)*math.sqrt((self.N-1)*f)
+		self.H1 = 0                                            
+		self.inhibition= self.H0 + self.lmbda*(self.s.sum() - self.f*self.N)
+		self.v = np.dot(self.W,self.s)-self.inhibition
 		
-	def update_states(self, xi=np.zeros(1)):
-		H0 = (self.N-1)*(self.f*self.W.mean() - self.theta/(self.N-1))
-		H1 = self.f*self.gamma*math.sqrt(self.N-1)
-		inhibition = H0 + H1*xi.sum()/(self.f*self.N) + self.lmbda*(self.s.sum() - self.f*self.N)
-		self.vi = np.dot(self.W, self.s) + self.gamma*math.sqrt(self.N)*xi - inhibition
-		self.s = self.activation_func(self.vi)
+	def update_states(self, external_input, nb_iter=1): #update neurons states
+		self.lmbda = self.W[self.mask].mean()
+		self.H0 = (self.N-1)*(self.f*self.W[self.mask].mean() - self.theta/(self.N-1)) + self.W[self.mask].std()*math.sqrt(2)*erfcinv(2*self.f)*math.sqrt((self.N-1)*self.f)
+		self.H1 = self.f*self.gamma*math.sqrt(self.N-1) 
+		for k in range(nb_iter):
+			self.inhibition = self.H0 + self.H1*external_input.sum()/(self.f*self.N) + self.lmbda*(self.s.sum() - self.f*self.N)
+			self.v = np.dot(self.W, self.s) + self.gamma*math.sqrt(self.N)*external_input - self.inhibition
+			self.s = self.activation_func(self.v)
 
-	def update_weights(self, learn_rate, epsilon):
-		theta0 = self.theta - (self.gamma + epsilon)*self.f*math.sqrt(self.N)
-		theta1 = self.theta + (self.gamma + epsilon)*self.f*math.sqrt(self.N)
-		coeffs = - (self.vi < self.theta) + (self.vi <= theta0) + (self.vi > self.theta) - (self.vi >= theta1)
-		self.W = self.W + coeffs*learn_rate*self.s
-
+	def Three_TLR(self, pattern, learn_rate, eps, nb_iter=100): #learning pattern
+		self.update_states(pattern) #update states to set the network into the presented pattern
+         #Define learning thresholds
+		theta0 = self.theta - (self.gamma + eps)*self.f*math.sqrt(self.N)
+		theta1 = self.theta + (self.gamma + eps)*self.f*math.sqrt(self.N)
+         #Update weights
+		for k in range(nb_iter):        
+			coeffs = - (self.v < self.theta).astype(int) + (self.v <= theta0).astype(int) + (self.v > self.theta).astype(int) - (self.v >= theta1).astype(int)
+			self.W = self.W + learn_rate*np.dot(np.reshape(coeffs,(coeffs.size,1)),np.reshape(self.s,(1,self.s.size)))
+			np.fill_diagonal(self.W, 0)
+			self.W = np.maximum(self.W, 0)
+			self.update_states(pattern)
